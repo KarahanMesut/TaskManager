06e3183b5a5e0f357b8ef2b7b15e3176b27196f9
Mesut KARAHAN <130998572+KarahanMesut@users.noreply.github.com>
2024-06-01 17:53:20 +0300

    2

---
+++ TaskManager.Business/TaskService.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TaskManager.Data;
+using TaskManager.Data.TaskManager.Data;
+
+namespace TaskManager.Business
+{
+    public class TaskService
+    {
+        private readonly TaskRepository taskRepository;
+
+        public TaskService(TaskRepository taskRepository)
+        {
+            this.taskRepository = taskRepository;
+        }
+
+        public Task<List<ToDoTask>> GetTasksAsync()
+        {
+            return taskRepository.GetAllTasksAsync();
+        }
+
+        public Task AddTaskAsync(ToDoTask task)
+        {
+            return taskRepository.AddTaskAsync(task);
+        }
+
+        public Task UpdateTaskAsync(ToDoTask task)
+        {
+            return taskRepository.UpdateTaskAsync(task);
+        }
+
+        public Task DeleteTaskAsync(int taskId)
+        {
+            return taskRepository.DeleteTaskAsync(taskId);
+        }
+
+        public async Task<List<ToDoTask>> GetTasksByUserAsync(int userId)
+        {
+            return await taskRepository.GetTasksByUserAsync(userId);
+        }
+
+        public Task ShareTaskAsync(int taskId, int userId)
+        {
+            return taskRepository.ShareTaskAsync(taskId, userId);
+        }
+
+        public Task AddCommentAsync(int taskId, int userId, string commentText)
+        {
+            return taskRepository.AddCommentAsync(taskId, userId, commentText);
+        }
+
+        public Task<List<Comment>> GetCommentsAsync(int taskId)
+        {
+            return taskRepository.GetCommentsAsync(taskId);
+        }
+    }
+
+}
---
+++ TaskManager.Business/UserService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Data;
+
+namespace TaskManager.Business
+{
+    public class UserService
+    {
+
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> AuthenticateUserAsync(string phoneNumber, string password)
+        {
+            var user = await _userRepository.GetUserByPhoneNumberAsync(phoneNumber);
+            if (user != null && VerifyPassword(password, user.PASSWORD))
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            user.PASSWORD = HashPassword(user.PASSWORD);
+            await _userRepository.AddUserAsync(user);
+        }
+
+        private string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var builder = new StringBuilder();
+                foreach (var b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private bool VerifyPassword(string inputPassword, string storedHash)
+        {
+            var hashInputPassword = HashPassword(inputPassword);
+            return hashInputPassword == storedHash;
+        }
+    }
+}
